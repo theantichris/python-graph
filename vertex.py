4abe6af0b4af52a56ec24bc1f1a80b87655424d8
Christopher Lamm <chris@theantichris.com>
2019-12-18 11:21:40 -0500

    prevening a vertex from having an edge to itself

--- vertex.py
+++ vertex.py
@@ -9,6 +9,9 @@ class Vertex:
     def get_edges(self):
         return list(self.edges.keys())
 
-    def add_edge(self, vertex, weight = 0):
-        print("Adding edge to {}".format(vertex))
-        self.edges[vertex.value] = weight
+    def add_edge(self, vertex, weight=0):
+        if vertex.value == self.value:
+            print("A vertex cannot have an edge to itself")
+        else:
+            print("Adding edge to {}".format(vertex))
+            self.edges[vertex.value] = weight
