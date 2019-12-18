221e46c36dc2479097c9c2df7698490cf5229100
Christopher Lamm <chris@theantichris.com>
2019-12-18 10:02:56 -0500

    added weight to edges

--- graph.py
+++ graph.py
@@ -7,8 +7,8 @@ class Graph:
         print("Adding {}".format(vertex.value))
         self.graph_dict[vertex.value] = vertex
 
-    def add_edge(self, from_vertex, to_vertex):
+    def add_edge(self, from_vertex, to_vertex, weight = 0):
         print("Adding edge from {} to {}".format(from_vertex, to_vertex))
-        self.graph_dict[from_vertex.value].add_edge(to_vertex.value)
+        self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
         if not self.directed:
-          self.graph_dict[to_vertex.value].add_edge(from_vertex.value)
+          self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
