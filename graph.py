fa52e70bb22ff0f652c887ea1e7c9c2cf5efbd4c
Christopher Lamm <chris@theantichris.com>
2019-12-18 10:49:36 -0500

    finding path between 2 vertices

--- graph.py
+++ graph.py
@@ -12,3 +12,17 @@ class Graph:
         self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
         if not self.directed:
           self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
+
+    def find_path(self, start_vertex, end_vertex):
+        print("Searching from {} to {}".format(start_vertex, end_vertex))
+        start = [start_vertex]
+        while len(start) > 0:
+            current_vertex = start.pop(0)
+            print(current_vertex)
+            if current_vertex == end_vertex:
+                return True
+            else:
+                vertex = self.graph_dict[current_vertex]
+                next_vertices = vertex.get_edges()
+                start.extend(next_vertices)
+        return False
