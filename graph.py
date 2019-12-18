a0d506dbc3e2eb22562b7ce67e54c123b37acba7
Christopher Lamm <chris@theantichris.com>
2019-12-18 09:57:35 -0500

    added add_edge() method

--- graph.py
+++ graph.py
@@ -6,3 +6,9 @@ class Graph:
     def add_vertex(self, vertex):
         print("Adding {}".format(vertex.value))
         self.graph_dict[vertex.value] = vertex
+
+    def add_edge(self, from_vertex, to_vertex):
+        print("Adding edge from {} to {}".format(from_vertex, to_vertex))
+        self.graph_dict[from_vertex.value].add_edge(to_vertex.value)
+        if not self.directed:
+          self.graph_dict[to_vertex.value].add_edge(from_vertex.value)
